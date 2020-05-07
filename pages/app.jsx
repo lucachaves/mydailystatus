933c39c134fad0c19b2626b914548a0a5e9fc118
Luiz Chaves <lucachaves@gmail.com>
2020-05-07 15:51:08 -0300

    add map

--- pages/app.jsx
+++ pages/app.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect } from 'react';
+import GoogleMapReact from 'google-map-react';
 import router from 'next/router';
 import auth0 from '../lib/auth0';
 import { db } from '../lib/db';
 import { distance } from '../lib/geo';
+
+const Marker = ({ text }) => <div>{text}</div>;
 
 const App = (props) => {
   useEffect(() => {
@@ -16,6 +19,7 @@ const App = (props) => {
   if (!props.isAuth || props.forceCreate) {
     return null;
   }
+  console.log();
 
   return (
     <div>
@@ -30,25 +34,38 @@ const App = (props) => {
           </tr>
         </thead>
         <tbody>
-          {props.checkins.map(
-            (checkin) =>
-              checkin.id === props.user.sub && (
-                <tr key={checkin.id}>
-                  <td className="border-collapse border-2 border-gray-500">
-                    {checkin.status}
-                  </td>
-                  <td className="border-collapse border-2 border-gray-500">
-                    {`${checkin.coords.lat}, ${checkin.coords.long}`}
-                  </td>
-                  <td className="border-collapse border-2 border-gray-500">
-                    {checkin.distance}
-                  </td>
-                </tr>
-              )
-          )}
+          {props.checkins.map((checkin) => (
+            <tr key={checkin.id}>
+              <td className="border-collapse border-2 border-gray-500">
+                {checkin.status}
+              </td>
+              <td className="border-collapse border-2 border-gray-500">
+                {`${checkin.coords.lat}, ${checkin.coords.long}`}
+              </td>
+              <td className="border-collapse border-2 border-gray-500">
+                {checkin.distance}
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
       {/* <pre>{JSON.stringify(props, null, 2)}</pre> */}
+      <div className="py-4" style={{ height: '400px', width: '400px' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: props.coords.key }}
+          defaultCenter={props.coords.center}
+          defaultZoom={props.coords.zoom}
+        >
+          {props.checkins.map((checkin) => (
+            <Marker
+              key={checkin.id}
+              lat={checkin.coords.lat}
+              lng={checkin.coords.long}
+              text={checkin.status}
+            />
+          ))}
+        </GoogleMapReact>
+      </div>
     </div>
   );
 };
@@ -83,8 +100,6 @@ export async function getServerSideProps({ req, res }) {
         })
         .get();
 
-      console.log(checkins);
-
       const checkinsList = [];
 
       checkins.docs.forEach((doc) => {
@@ -108,6 +123,14 @@ export async function getServerSideProps({ req, res }) {
         props: {
           isAuth: true,
           user: session.user,
+          coords: {
+            center: {
+              lat: todaysData.coordinates.latitude,
+              lng: todaysData.coordinates.longitude,
+            },
+            zoom: 15,
+            key: process.env.GOOGLE_MAPS_KEY,
+          },
           forceCreate: false,
           checkins: checkinsList,
         },
